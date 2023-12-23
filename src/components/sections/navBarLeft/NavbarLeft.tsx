8f17e7cc0c00a622b89110bf7b40cbf496175ad2
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-23 11:24:18 +0300

    23.12 - 20 lesson

--- src/components/sections/navBarLeft/NavbarLeft.tsx
+++ src/components/sections/navBarLeft/NavbarLeft.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import classes from './NavbarLeft.module.css'
+import {NavLink} from 'react-router-dom'
 
 const NavbarLeft: React.FC = () => {
     return (
         <nav className={classes.nav}>
             <ul>
-                <li className={classes.item}><a href={'/profile'}>Profile</a></li>
-                <li className={classes.item}><a href={'/messages'}>Messages</a></li>
-                <li className={classes.item}><a href={'/news'}>News</a></li>
-                <li className={classes.item}><a href={'/music'}>Music</a></li>
+                <li className={classes.item}><NavLink to={'/profile'} activeClassName={classes.active}>Profile</NavLink></li>
+                <li className={classes.item}><NavLink to={'/messages'} activeClassName={classes.active}>Messages</NavLink></li>
+                <li className={classes.item}><NavLink to={'/news'} activeClassName={classes.active}>News</NavLink></li>
+                <li className={classes.item}><NavLink to={'/music'} activeClassName={classes.active}>Music</NavLink></li>
 
-                <li className={classes.item}><a href={'/settings'}>Settings</a></li>
+                <li className={classes.item}><NavLink to={'/settings'} activeClassName={classes.active}>Settings</NavLink></li>
             </ul>
         </nav>
     )
