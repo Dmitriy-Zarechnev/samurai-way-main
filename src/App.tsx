851f8783c9ed4703e56ec564a1f9f2379287c2c5
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-01-10 14:23:11 +0300

    10.01.2024 - 39lesson - 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import NavbarLeft from './components/sections/navBarLeft/NavbarLeft'
 import Main from './components/sections/main/Main'
 import {SummaryStatePropsType} from './redux/state'
 
-
 function App(props: SummaryStatePropsType) {
     return (
         <BrowserRouter>
