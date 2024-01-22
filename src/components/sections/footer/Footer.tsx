5e38fed7fe86c43801f2834d5cfed2058427719b
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-01-22 10:18:43 +0300

    22.01.2024 - refactor - 1

--- src/components/sections/footer/Footer.tsx
+++ src/components/sections/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../assets/images/logo.svg'
+import logo from '../../../assets/images/logo.svg'
 import classes from './Footer.module.css'
 
 const Footer: React.FC = () => {
