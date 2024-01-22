90d8b7b15b0e8a19cd19e7803a3a563b83301987
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-01-22 14:15:25 +0300

    22.01.2024 - 49 lesson - 5

--- src/components/sections/main/users/Users.tsx
+++ src/components/sections/main/users/Users.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {UsersListType} from '../../../../redux/redux-store'
 import classes from './Users.module.css'
 
+
 type UsersPropsType = {
     users: UsersListType[],
     followFriend: (userID: number) => void,
@@ -11,33 +12,48 @@ type UsersPropsType = {
 
 
 const Users: React.FC<UsersPropsType> = (props) => {
+
+    // if (props.users.length === 0) {
+    //     props.setUsers([
+    //         {id: 1, fullName: 'dz', status: 'I am using react', followed: true, photoURL: min, location: {city: 'Zenon', country: 'Madagaskar'}},
+    //         {id: 2, fullName: 'fc', status: 'I am using css', followed: false, photoURL: min, location: {city: 'Hell', country: 'Haven'}},
+    //         {id: 3, fullName: 'gr', status: 'I am using html', followed: true, photoURL: min, location: {city: 'Paradice', country: 'Haven'}},
+    //         {id: 4, fullName: 'ld', status: 'I am using bll', followed: false, photoURL: min, location: {city: 'Moscow', country: 'Zelda'}}
+    //     ])
+    // }
+
     return (
-        <div>
+        <div className={classes.users_lists}>
             {props.users.map(el => {
                 return (
-                    <div key={el.id}>
-                        <div>
-                            <img src={el.photoURL} alt="img" className={classes.img}/>
-                            {el.followed
-                                ? <button onClick={() => {
-                                    props.unfollowFriend(el.id)
-                                }}>
-                                    UnFollow
-                                </button>
-                                : <button onClick={() => {
-                                    props.followFriend(el.id)
-                                }}>
-                                    Follow
-                                </button>}
+                    <div key={el.id} className={classes.users_list}>
+                        <div className={classes.users_up_list}>
+                            <img src={el.photoURL} alt="img" className={classes.users_up_list__img}/>
+                            <p className={classes.users_up_list__status}>{el.status}</p>
                         </div>
-                        <div>
-                            <div>
-                                <h4>{el.fullName}</h4>
-                                <p>{el.status}</p>
+                        <div className={classes.users_down_list}>
+                            <div className={classes.users_down_list__left}>
+                                <h4 className={classes.users_down_list__left_fullName}>{el.fullName}</h4>
+                                <div className={classes.users_down_list__left_down}>
+                                    <span className={classes.users_down_list__left_country}>{el.location.country}</span>
+                                    <span className={classes.users_down_list__left_city}>{el.location.city}</span>
+                                </div>
                             </div>
-                            <div>
-                                <span>{el.location.city}</span>
-                                <span>{el.location.country}</span>
+                            <div className={classes.users_down_list__right}>
+                                {el.followed
+                                    ?
+                                    <button onClick={() => {
+                                        props.unfollowFriend(el.id)
+                                    }}>
+                                        UnFollow
+                                    </button>
+                                    :
+                                    <button onClick={() => {
+                                        props.followFriend(el.id)
+                                    }}>
+                                        Follow
+                                    </button>
+                                }
                             </div>
                         </div>
                     </div>
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -5,17 +5,20 @@ const FOLLOW_FRIEND = 'FOLLOW-FRIEND'
 const UNFOLLOW_FRIEND = 'UNFOLLOW-FRIEND'
 const SET_USERS = 'SET-USERS'
 
-let initialState: UsersInitialState = {
+let initialState: UsersInitialState  = {
+
     users: [
         {id: 1, fullName: 'dz', status: 'I am using react', followed: true, photoURL: min, location: {city: 'Zenon', country: 'Madagaskar'}},
         {id: 2, fullName: 'fc', status: 'I am using css', followed: false, photoURL: min, location: {city: 'Hell', country: 'Haven'}},
         {id: 3, fullName: 'gr', status: 'I am using html', followed: true, photoURL: min, location: {city: 'Paradice', country: 'Haven'}},
         {id: 4, fullName: 'ld', status: 'I am using bll', followed: false, photoURL: min, location: {city: 'Moscow', country: 'Zelda'}}
     ]
+
+    // users: []
 }
 
 
-export const usersReducer = (state: UsersInitialState = initialState, action: ActionType) => {
+export const usersReducer = (state: UsersInitialState  = initialState, action: ActionType) => {
 
     switch (action.type) {
         case FOLLOW_FRIEND:
