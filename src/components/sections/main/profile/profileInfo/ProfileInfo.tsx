e2771c633622ef25c7e870d648e297fd3ea3678d
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-02-02 12:48:17 +0300

    02.02.2024 - 59 lesson - 5

--- src/components/sections/main/profile/profileInfo/ProfileInfo.tsx
+++ src/components/sections/main/profile/profileInfo/ProfileInfo.tsx
@@ -9,33 +9,37 @@ import vk from '../../../../../assets/images/vk.svg'
 export const ProfileInfo = (props: { profileInfo: ProfileInfoType | null }) => {
     return (
         <>
-            <Preloader isFetching={!props.profileInfo}/>
-            <div className={S.profile}>
-                <div className={S.profile__box}>
-                    <div className={S.profile__left}>
-                        <p className={S.profile__full_name}>
-                            {props.profileInfo.fullName}
-                        </p>
-                        <p className={S.profile_info__text}>
-                            {props.profileInfo.aboutMe}
-                        </p>
-                        <div className={S.profile__work_box}>
-                            <p className={S.profile_info__text}>
-                                {props.profileInfo.lookingForAJobDescription}
+            {props.profileInfo
+                ? <div className={S.profile}>
+                    <div className={S.profile__box}>
+                        <div className={S.profile__left}>
+                            <p className={S.profile__full_name}>
+                                {props.profileInfo.fullName}
                             </p>
-                            <span>{props.profileInfo.lookingForAJob ? '🔍' : '🐱‍👤'}</span>
+                            <p className={S.profile_info__text}>
+                                {props.profileInfo.aboutMe}
+                            </p>
+                            <div className={S.profile__work_box}>
+                                <p className={S.profile_info__text}>
+                                    {props.profileInfo.lookingForAJobDescription}
+                                </p>
+                                <span>{props.profileInfo.lookingForAJob ? '🔍' : '🐱‍👤'}</span>
+                            </div>
                         </div>
+                        <img className={S.profile__img} src={props.profileInfo.photos.large} alt={`${props.profileInfo.fullName} - avatar should be here`}/>
                     </div>
-                    <img className={S.profile__img} src={props.profileInfo.photos.large} alt={`${props.profileInfo.fullName} - avatar should be here`}/>
+                    <div className={S.profile__contacts}>
+                        <a href={props.profileInfo.contacts.vk}><img className={S.profile__contacts_img} src={props.profileInfo.contacts.vk && vk} alt="vk"/>
+                        </a>
+                        <a href={props.profileInfo.contacts.github}>
+                            <img className={S.profile__contacts_img} src={props.profileInfo.contacts.github && git} alt="git"/>
+                        </a>
+                    </div>
                 </div>
-                <div className={S.profile__contacts}>
-                    <a href={props.profileInfo.contacts.vk}><img className={S.profile__contacts_img} src={props.profileInfo.contacts.vk && vk} alt="vk"/>
-                    </a>
-                    <a href={props.profileInfo.contacts.github}>
-                        <img className={S.profile__contacts_img} src={props.profileInfo.contacts.github && git} alt="git"/>
-                    </a>
-                </div>
-            </div>
+                : <Preloader isFetching={!props.profileInfo}/>
+            }
+
+
         </>
     )
 }
