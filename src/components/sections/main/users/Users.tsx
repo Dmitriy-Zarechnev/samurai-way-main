ce7909b3ec7a2d61134eb0f2c6df44b6bcc01f49
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-02-06 07:31:24 +0300

    06.02 - 62 lesson - 5

--- src/components/sections/main/users/Users.tsx
+++ src/components/sections/main/users/Users.tsx
@@ -9,28 +9,6 @@ import axios from 'axios'
 
 export const Users = (props: UsersPropsType) => {
 
-    // let pagesCount = Math.ceil(props.totalCount / props.pageSize)
-    // let pages = []
-    //
-    // for (let i = 1; i <= pagesCount; i++) {
-    //     pages.push(i)
-    // }
-    //
-    // let curPg = props.currentPage
-    // let curPF = ((curPg - 5) < 0) ? 0 : curPg - 5
-    // let curPL = curPg + 5
-    //
-    // let pagStart = pages.slice(0, 3)
-    // let pagCenter = pages.slice(curPF, curPL)
-    // let pagEnd = pages.slice(pages.length - 3)
-    //
-    // if (curPg <= 7) {
-    //     pagStart = []
-    // }
-    // if (curPg >= pages.length - 7) {
-    //     pagEnd = []
-    // }
-
     return (
         <div className={S.users_lists}>
             <div className={S.users_lists__pagination}>
